e0cbaed3df8cce516fa87af803f973c550cad222
Anton Davydov <antondavydov.o@gmail.com>
2018-04-03 18:11:09 +0300

    Find link in db and use it for redirection

--- apps/web/controllers/links/show.rb
+++ apps/web/controllers/links/show.rb
@@ -2,8 +2,18 @@ module Web::Controllers::Links
   class Show
     include Web::Action
 
+    attr_reader :repo
+
+    def initialize(repo: LinkRepository.new)
+      @repo = repo
+    end
+
     def call(params)
-      redirect_to 'google.com'
+      if link = repo.find_by_key(params[:id])
+        redirect_to link.url
+      else
+        self.body = 'Not found'
+      end
     end
   end
 end
--- spec/web/controllers/links/show_spec.rb
+++ spec/web/controllers/links/show_spec.rb
@@ -1,9 +1,30 @@
-RSpec.describe Web::Controllers::Links::Show, type: :action do
-  let(:action) { described_class.new }
+RSpec.describe Web::Controllers::Links::Show do
+  let(:action) { described_class.new(repo: link_repo) }
   let(:params) { Hash[id: 1] }
+  let(:link_repo) { double(:link_repo, find_by_key: entity) }
 
-  it 'redirects to google' do
-    response = action.call(params)
-    expect(response).to redirect_to('google.com')
+  context 'when link exist' do
+    let(:entity) { Link.new(url: 'google.com') }
+
+    it { expect(action.call(params)).to redirect_to('google.com') }
+  end
+
+  context 'when link does not exist' do
+    let(:entity) { nil }
+
+    it 'returns error message' do
+      response = action.call(params)
+      expect(response[2]).to eq ['Not found']
+    end
+  end
+
+  context 'when action calls real repository' do
+    let(:link_repo) { LinkRepository.new }
+    let(:params) { Hash[id: link.key] }
+    let(:link) { link_repo.create(key: '123', url: 'google.com') }
+
+    after { link_repo.clear }
+
+    it { expect(action.call(params)).to redirect_to('google.com') }
   end
 end
